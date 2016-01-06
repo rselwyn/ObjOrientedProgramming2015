382fcee2d2c550a82784ecadb3b27bffce8b93da
Robbie S <robbie@selwynsoftware.com>
2016-01-06 13:31:26 -0800

    Added TestAuthor and completed 1.2

---
+++ Ch1-2/src/TestAuthor.java
@@ -0,0 +1,11 @@
+
+public class TestAuthor {
+
+	public static void main(String[] args) {
+		//Author class tester
+		Author auth = new Author("Author", "Email", "m".charAt(0));
+		//print email test
+		System.out.println(auth.getEmail());
+	}
+
+}
