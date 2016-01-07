e44fce1f28397d0425614231e2fa7c834457fec5
Robbie S <robbie@selwynsoftware.com>
2016-01-07 11:58:34 -0800

    Added myPoint (1.3)

---
+++ Ch1-3/src/MyPoint.java
@@ -0,0 +1,67 @@
+
+public class MyPoint {
+	private int x;
+	private int y;
+	/*
+	 * No argument constructor that sets the point a t (0,0)
+	 */
+	public MyPoint(){
+		setXY(0,0);
+	}
+	
+	/*
+	 * The constructor that sets the point at a given point
+	 */
+	public MyPoint(int x, int y){
+		//Set x and y
+		setXY(x,y);
+	}
+	
+	
+	
+	/*
+	 * Set the coordinates
+	 * @param x: the x coordinate
+	 * @param yL the y coordinate
+	 */
+	public void setXY(int x, int y){
+		setX(x);
+		setY(y);
+	}
+	
+	/*
+	 * @param x
+	 */
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	/*
+	 * @param y: the y value
+	 */
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	/*
+	 * @return x: the x instance variable
+	 */
+	public int getX(){
+		return this.x;
+	}
+	
+	/*
+	 * @return y: the y instance variable
+	 */
+	public int getY(){
+		return this.y;
+	}
+	
+	
+	/*
+	 * toString method
+	 */
+	public String toString(){
+		return "("+this.x+","+this.y+")";
+	}
+}
