e70b9245e5d0fe84ea124ffa03dafea9dea710e4
Robbie S <robbie@selwynsoftware.com>
2016-01-09 18:15:51 -0800

    Added MyComplex

---
+++ Ch1-6/src/MyComplex.java
@@ -0,0 +1,31 @@
+
+public class MyComplex {
+	private double real;
+	private double imag;
+	
+	/*
+	 * @return the real instance variable
+	 */
+	public double getReal(){
+		return this.real;
+	}
+	/*
+	 * @param real: the real number to set the instance variable
+	 */
+	public void setReal(double real){
+		this.real = real;
+	}
+	/*
+	 * @return the imaginary instance variable
+	 */
+	public double getImag(){
+		return this.imag;
+	}
+	/*
+	 * @param the double coefficient of the imaginary number
+	 */
+	public void setImag(double imag){
+		this.imag = imag;
+	}
+	
+}
