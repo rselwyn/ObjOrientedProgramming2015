b6ada4b656061aaab94d9b9224155a6918c02175
Robbie S <robbie@selwynsoftware.com>
2016-01-15 09:59:12 -0800

    Changed some other projects.  Lots of work on 1.6

--- Ch1-6/src/MyComplex.java
+++ Ch1-6/src/MyComplex.java
@@ -3,6 +3,20 @@ public class MyComplex {
 	private double real;
 	private double imag;
 	
+	public MyComplex(double real, double imag){
+		setValue(real,imag);
+	}
+	
+	/*
+	 * @param real: the real number
+	 * @param imag: the imaginary coefficient
+	 */
+	public void setValue(double real, double imag){
+		setReal(real);
+		setImag(imag);
+	}
+	
+	
 	/*
 	 * @return the real instance variable
 	 */
@@ -28,4 +42,89 @@ public class MyComplex {
 		this.imag = imag;
 	}
 	
+	/*
+	 * @return the toString from online
+	 */
+	public String toString(){
+		return "("+getReal()+" + "+getImag()+"i )";
+	}
+	
+	/*
+	 * @return if imag ==0
+	 */
+	public boolean isReal(){
+		return getImag() == 0;
+	}
+	
+	/*
+	 * @return is the number imaginary (if the imaginary coefficient != 0)
+	 */
+	public boolean isImaginary(){
+		return getImag()!=0;
+	}
+	
+	/*
+	 * See if the complex number is equal to another complex number
+	 * @param real: the real part of the comparing number
+	 * @param imag: the imaginary part of the comparing number
+	 */
+	public boolean equals(double real, double imag){
+		return getReal()==real && getImag() == imag;
+	}
+	
+	/*
+	 * See if this MyComplex object is equal to another one
+	 * @param the other mycomplex object
+	 */
+	public boolean equals(MyComplex mc){
+		return equals(mc.getReal(), mc.getImag());
+	}
+	
+	/*
+	 * Returns the magnitude of the complex number
+	 * @return the magnitude
+	 */
+	public double magnitutde(){
+		return Math.sqrt(Math.pow(getImag(),2) + Math.pow(getReal(), 2));
+	}
+	
+	/*
+	 * Returns the argument in radians
+	 * Formula from the website
+	 * @return the argument in radians
+	 */
+	public double argumentInRadians(){
+		return Math.atan2(getImag(), getReal());
+	}
+	
+	/*
+	 * argumentInRadians but we convert radians to degrees
+	 * @return the argumentInDegrees
+	 */
+	public int argumentInDegrees(){
+		return (int) ((180/Math.PI)*argumentInRadians());
+	}
+	/*
+	 * Return the conjugate
+	 * @return the conjugate as a mycomplex
+	 */
+	public MyComplex conjugate(){
+		return new MyComplex(getReal(), -getImag());
+	}
+	
+	/*
+	 * Returns a complex number that is the sum of two complex numbers
+	 * @return the sum of the complex numbers
+	 */
+	public MyComplex add(MyComplex p){
+		return new MyComplex((p.getReal()+getReal()), (p.getImag()+getImag()));
+	}
+	
+	/*
+	 * Returns a complex number that is the difference of two complex numbers
+	 * @return the difference of the complex numbers
+	 */
+	public MyComplex subtact(MyComplex p){
+		return new MyComplex((p.getReal()-getReal()), (p.getImag()-getImag()));
+	}
 }
