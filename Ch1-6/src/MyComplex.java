843678a988d5264578110265e2a4d1b2d4aa9283
Robbie S <robbie@selwynsoftware.com>
2016-01-15 12:24:08 -0800

    potentially finished the my complex class

--- Ch1-6/src/MyComplex.java
+++ Ch1-6/src/MyComplex.java
@@ -7,7 +7,7 @@ public class MyComplex {
 		setValue(real,imag);
 	}
 	
-	/*
+	/**
 	 * @param real: the real number
 	 * @param imag: the imaginary coefficient
 	 */
@@ -17,53 +17,53 @@ public class MyComplex {
 	}
 	
 	
-	/*
+	/**
 	 * @return the real instance variable
 	 */
 	public double getReal(){
 		return this.real;
 	}
-	/*
+	/**
 	 * @param real: the real number to set the instance variable
 	 */
 	public void setReal(double real){
 		this.real = real;
 	}
-	/*
+	/**
 	 * @return the imaginary instance variable
 	 */
 	public double getImag(){
 		return this.imag;
 	}
-	/*
+	/**
 	 * @param the double coefficient of the imaginary number
 	 */
 	public void setImag(double imag){
 		this.imag = imag;
 	}
 	
-	/*
+	/**
 	 * @return the toString from online
 	 */
 	public String toString(){
 		return "("+getReal()+" + "+getImag()+"i )";
 	}
 	
-	/*
+	/**
 	 * @return if imag ==0
 	 */
 	public boolean isReal(){
 		return getImag() == 0;
 	}
 	
-	/*
+	/**
 	 * @return is the number imaginary (if the imaginary coefficient != 0)
 	 */
 	public boolean isImaginary(){
 		return getImag()!=0;
 	}
 	
-	/*
+	/**
 	 * See if the complex number is equal to another complex number
 	 * @param real: the real part of the comparing number
 	 * @param imag: the imaginary part of the comparing number
@@ -72,7 +72,7 @@ public class MyComplex {
 		return getReal()==real && getImag() == imag;
 	}
 	
-	/*
+	/**
 	 * See if this MyComplex object is equal to another one
 	 * @param the other mycomplex object
 	 */
@@ -80,7 +80,7 @@ public class MyComplex {
 		return equals(mc.getReal(), mc.getImag());
 	}
 	
-	/*
+	/***
 	 * Returns the magnitude of the complex number
 	 * @return the magnitude
 	 */
@@ -88,7 +88,7 @@ public class MyComplex {
 		return Math.sqrt(Math.pow(getImag(),2) + Math.pow(getReal(), 2));
 	}
 	
-	/*
+	/**
 	 * Returns the argument in radians
 	 * Formula from the website
 	 * @return the argument in radians
@@ -97,14 +97,14 @@ public class MyComplex {
 		return Math.atan2(getImag(), getReal());
 	}
 	
-	/*
+	/**
 	 * argumentInRadians but we convert radians to degrees
 	 * @return the argumentInDegrees
 	 */
 	public int argumentInDegrees(){
 		return (int) ((180/Math.PI)*argumentInRadians());
 	}
-	/*
+	/**
 	 * Return the conjugate
 	 * @return the conjugate as a mycomplex
 	 */
@@ -112,7 +112,17 @@ public class MyComplex {
 		return new MyComplex(getReal(), -getImag());
 	}
 	
-	/*
+	/**
+	 * Return the conjugate
+	 * @return the conjugate of the parameter
+	 */
+	public MyComplex conjugate(MyComplex con){
+		return new MyComplex(con.getReal(), -con.getImag());
+	}
+	
+	
+	
+	/**
 	 * Returns a complex number that is the sum of two complex numbers
 	 * @return the sum of the complex numbers
 	 */
@@ -120,11 +130,36 @@ public class MyComplex {
 		return new MyComplex((p.getReal()+getReal()), (p.getImag()+getImag()));
 	}
 	
-	/*
+	/**
 	 * Returns a complex number that is the difference of two complex numbers
 	 * @return the difference of the complex numbers
 	 */
 	public MyComplex subtact(MyComplex p){
 		return new MyComplex((p.getReal()-getReal()), (p.getImag()-getImag()));
 	}
+	
+	/**
+	 * Multiply with another complex number
+	 * @param the complex number to multiply by
+	 */
+	public MyComplex multiplyWith(MyComplex another){
+		int ac = (int) (another.getReal() * getReal());
+		int bd = (int) (another.getImag() * getImag());
+		int ad = (int) (another.getReal() * getImag());
+		int bc = (int) (another.getImag() * getReal());
+		return new MyComplex((ac-bc), (ad-bc));
+	}
+	/**
+	 * Return the mycomplex divided by another number
+	 * @param num: the the complex number to divide the object by
+	 * @return the divided complex number
+	 */
+	public MyComplex divideBy(MyComplex num){
+		double cd = Math.pow(num.getImag(),2) + Math.pow(num.getReal(), 2); //get the cd value
+//		return new MyComplex();
+		MyComplex conjMult = multiplyWith(conjugate(num)); //find the conjugate
+		MyComplex mult = multiplyWith(conjMult); //multiply  the conjugate by the objcet
+		return new MyComplex(mult.getReal()/cd, mult.getImag()/cd); //return mult divided by cd val
+	}
+	
 }
