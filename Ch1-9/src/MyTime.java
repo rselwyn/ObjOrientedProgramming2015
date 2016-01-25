b6f2e7e37160b43bf939429565091612b71eee34
Robbie S <robbie@selwynsoftware.com>
2016-01-25 13:26:04 -0800

    Potentially done with 1.9

--- Ch1-9/src/MyTime.java
+++ Ch1-9/src/MyTime.java
@@ -5,6 +5,18 @@ public class MyTime {
 	private int seconds = 0;
 	
 	/**
+	 * Constructor for the object
+	 * @param hours
+	 * @param minutes
+	 * @param seconds
+	 */
+	public MyTime(int hours, int minutes, int seconds){
+		setHours(hours);
+		setMinute(minutes);
+		setSeconds(seconds);
+	}
+	
+	/**
 	 * Get the hours
 	 * @return the hours
 	 */
@@ -59,10 +71,136 @@ public class MyTime {
 	/**
 	 * Changes the second to the next second
 	 */
-	public void nextSecond(){
+	public MyTime nextSecond(){
+		int second = getSeconds();
+		int minute = getMinute();
+		int hour = getHours();
+		if (second != 59){
+			second++; 
+		}
+		else{
+			if (minute!=59){
+				minute++;
+			}
+			else{
+				minute = 0;
+				if (hours!=23){
+					hour++;
+				}
+				else{
+					hour = 0;
+				}
+			}
+			second = 0;
+		}
 		
+		return new MyTime(hour,minute,second);
 	}
 	
+	/**
+	 * Changes the minute to the next minute
+	 */
+	public MyTime nextMinute(){
+		int second = getSeconds();
+		int minute = getMinute();
+		int hour = getHours();
+		
+		if (minute!=59){
+			minute++;
+		}
+		else{
+			minute = 0;
+			if (hours!=23){
+				hour++;
+			}
+			else{
+				hour = 0;
+			}
+		}
+		second = 0;
 	
+		
+		return new MyTime(hour,minute,second);
+	}
 	
+	/**
+	 * Changes the second to the next second
+	 */
+	public MyTime nextHour(){
+		int second = getSeconds();
+		int minute = getMinute();
+		int hour = getHours();
+		
+		if (hours!=23){
+			hour++;
+		}
+		else{
+			hour = 0;
+		}
+		
+		return new MyTime(hour,minute,second);
+	}
+	
+	public MyTime previousSecond(){
+		int second = getSeconds();
+		int minute = getMinute();
+		int hour = getHours();
+		if (second != 0){
+			second--;
+		}
+		else{
+			seconds=59;
+			if (minute == 0){
+				if (hour == 0){
+					hour = 23;
+				}
+				else{
+					hour--;
+				}
+			}
+			else{
+				minute--;
+			}	
+		}
+		
+		return new MyTime(hour,minute,second);
+	}
+
+	public MyTime previousMinute(){
+		int second = getSeconds();
+		int minute = getMinute();
+		int hour = getHours();
+
+		if (minute == 0){
+			minute = 59;
+			
+			if (hour == 0){
+				hour = 23;
+			}
+			else{
+				hour--;
+			}
+		}
+		else{
+			minute--;
+		}	
+
+		return new MyTime(hour,minute,second);
+	}
+	
+	public MyTime previouseHour(){
+		int second = getSeconds();
+		int minute = getMinute();
+		int hour = getHours();
+
+		if (hour == 0){
+			hour = 23;
+		}
+		else{
+			hour--;
+		}
+
+
+		return new MyTime(hour,minute,second);
+	}
 }
