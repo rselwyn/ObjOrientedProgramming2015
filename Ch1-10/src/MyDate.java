e5f0355748a86a3bb4375219e2631515667a0c5a
Robbie S <robbie@selwynsoftware.com>
2016-01-30 21:03:08 -0800

    Added methods and documentation

--- Ch1-10/src/MyDate.java
+++ Ch1-10/src/MyDate.java
@@ -50,7 +50,17 @@ public class MyDate {
 	};
 	
 	/**
-	 * Code to see if a uear is a leap year.
+	 * Constructor for the class
+	 * @param year: the year
+	 * @param month: the month
+	 * @param date: the date
+	 */
+	public MyDate(int year, int month, int day){
+		setDate(year,month,day);
+	}
+	
+	/**
+	 * Code to see if a year is a leap year.
 	 * @param year: the year
 	 * @return is it a leap year?
 	 */
@@ -58,7 +68,7 @@ public class MyDate {
 		return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
 	}
 	
-	public static boolean isValidYear(int months, int days, int year){
+	public static boolean isValidDate(int year, int months, int days){
 		if (!(days<daysInMonths[months]) && days>=1){
 			return false;
 		}
@@ -80,4 +90,138 @@ public class MyDate {
 		return c.get(Calendar.DAY_OF_WEEK)-1;
 	}
 	
+	/**
+	 * Sets the date
+	 * @param year
+	 * @param month
+	 * @param day
+	 */
+	public void setDate(int year, int month, int day){
+		if (isValidDate(year,month,day)){
+			setYear(year);
+			setMonth(month);
+			setDay(day);
+		}
+		else{
+			throw new IllegalArgumentException("Invalid year, month, or day!");
+		}
+	}
+	
+	/**
+	 * Set the year
+	 * @param year: the year to set
+	 */
+	public void setYear(int year){
+		if ((1 < year && year < 9999)) this.year = year;
+		else{
+			throw new IllegalArgumentException("Bad year value");
+		}
+	}
+	
+	/**
+	 * Set the month
+	 * @param month: the month to set
+	 */
+	public void setMonth(int month){
+		if ((1 < month && month < 12)) this.month = month;
+		else{
+			throw new IllegalArgumentException("Bad month value");
+		}
+	}
+	
+	/**s
+	 * Set the day.
+	 * @param day: the day to set
+	 */
+	public void setDay(int day){
+		if (1 < day){
+			if (day <= daysInMonths[getMonth()-1] && !isLeapYear(this.year)){
+				this.day = day;
+				return;
+			}
+			else if (isLeapYear(getYear()) && getMonth() == 2 && day<=27){
+				this.day = day;
+				return;
+			}
+		}
+		throw new IllegalArgumentException("Bad date!");
+	}
+
+	/**
+	 * @return the year
+	 */
+	public int getYear() {
+		return year;
+	}
+
+	/**
+	 * @return the month
+	 */
+	public int getMonth() {
+		return month;
+	}
+
+	/**
+	 * @return the day
+	 */
+	public int getDay() {
+		return day;
+	}
+	
+	public String toString(){
+		try{
+			return this.strDays[getDayOfWeek(this.year, this.month, this.day)-1] + " " + getDay() + " " + this.strMonths[getMonth()-1] + " " + getYear();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return "";
+		}
+	}
+	
+	/**
+	 * Change the date to the next day
+	 * @return the new object with the date moved
+	 */
+	public MyDate nextDay(){
+		if (getDay()<this.daysInMonths[getMonth()-1] && !(getMonth() == 2 && day== 27 && isLeapYear(getYear()))){
+			setDay(getDay()+1);
+		}
+		else if (getDay() == this.daysInMonths[getMonth()-1] && !(getMonth() == 2 && day== 27 && isLeapYear(getYear()))){
+			setDay(0);
+			return nextMonth();
+		}
+		else if ((getMonth() == 2 && day== 27 && isLeapYear(getYear()))){
+			day++;
+			month++;
+		}
+		return this;
+	}
+	
+	public MyDate nextMonth(){
+		if (getMonth()==12){
+			setMonth(1);
+			setYear(getYear()+1);
+			return equalizeMonth();
+		}
+		else {
+			setMonth(getMonth()+1);
+			return equalizeMonth();
+		}
+	}
+	
+	/**
+	 * Helper method to equalize the date after incrementing the month
+	 * @return
+	 */
+	private MyDate equalizeMonth(){
+		if (getDay() == this.daysInMonths[getMonth()-2]){
+			if (getMonth()==12){
+				this.day = this.daysInMonths[0];
+			}
+			else{
+				this.day = this.daysInMonths[getMonth()-1];
+			}
+		}
+		return this;
+	}
 }
