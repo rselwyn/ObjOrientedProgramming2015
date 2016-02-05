5d7e9363d12faca9d72849832a5ce7bd756b00e5
Robbie S <robbie@selwynsoftware.com>
2016-02-05 12:47:30 -0800

    Finished 1.10

--- Ch1-10/src/MyDate.java
+++ Ch1-10/src/MyDate.java
@@ -68,7 +68,16 @@ public class MyDate {
 		return (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
 	}
 	
+	/**
+	 * 
+	 * @param year the year
+	 * @param months the month
+	 * @param days the day
+	 * @return whether or not the day was a leap year
+	 */
 	public static boolean isValidDate(int year, int months, int days){
+		if (!isLeapYear(year) && months == 2 && days == 29) return false;
+		
 		if (!(days<daysInMonths[months]) && days>=1){
 			return false;
 		}
@@ -80,15 +89,24 @@ public class MyDate {
 		return true;
 	}
 	
+	/**
+	 * Gets the day of week
+	 * @param year: the year
+	 * @param month: the month
+	 * @param day: the day
+	 * @return the day of the week
+	 * @throws ParseException
+	 */
 	public static int getDayOfWeek(int year, int month, int day) throws ParseException{
 		Calendar c = Calendar.getInstance();
 		c.setTime(new SimpleDateFormat("dd/M/yyyy").parse(day+"/"+month+"/"+year));
 		/*
 		 * The builtin returns 1 as sunday, so we have to change it to 0 for sunday, 1 for monday, etc.
 		 */
-		if (c.get(Calendar.DAY_OF_WEEK) == 1) return 7;
 		return c.get(Calendar.DAY_OF_WEEK)-1;
 	}
+	
+ 
 	
 	/**
 	 * Sets the date
@@ -123,7 +141,7 @@ public class MyDate {
 	 * @param month: the month to set
 	 */
 	public void setMonth(int month){
-		if ((1 < month && month < 12)) this.month = month;
+		if ((1 <= month && month <= 12)) this.month = month;
 		else{
 			throw new IllegalArgumentException("Bad month value");
 		}
@@ -134,12 +152,19 @@ public class MyDate {
 	 * @param day: the day to set
 	 */
 	public void setDay(int day){
-		if (1 < day){
+		if (1 <= day){
+			
+			//normal day
 			if (day <= daysInMonths[getMonth()-1] && !isLeapYear(this.year)){
 				this.day = day;
 				return;
 			}
+			//leap year and the day
 			else if (isLeapYear(getYear()) && getMonth() == 2 && day<=27){
+				this.day = day;
+				return;
+			}
+			else {
 				this.day = day;
 				return;
 			}
@@ -173,7 +198,7 @@ public class MyDate {
 	 */
 	public String toString(){
 		try{
-			return this.strDays[getDayOfWeek(this.year, this.month, this.day)-1] + " " + getDay() + " " + this.strMonths[getMonth()-1] + " " + getYear();
+			return this.strDays[getDayOfWeek(this.year, this.month, this.day)] + " " + getDay() + " " + this.strMonths[getMonth()-1] + " " + getYear();
 		}
 		catch(Exception e){
 			e.printStackTrace();
@@ -186,20 +211,30 @@ public class MyDate {
 	 * @return the new object with the date moved
 	 */
 	public MyDate nextDay(){
+		//normal day and it is not a feb 28
 		if (getDay()<this.daysInMonths[getMonth()-1] && !(getMonth() == 2 && day== 28 && isLeapYear(getYear()))){
 			setDay(getDay()+1);
 		}
+		//last day of the month, 
 		else if (getDay() == this.daysInMonths[getMonth()-1] && !(getMonth() == 2 && day== 28 && isLeapYear(getYear()))){
-			setDay(0);
+			setDay(1);
 			return nextMonth();
 		}
 		else if ((getMonth() == 2 && day== 28 && isLeapYear(getYear()))){
 			day++;
-			month++;
+		}
+		else if ((getMonth() == 2 && day== 29 && isLeapYear(getYear()))){
+			setDay(1);
+			setMonth(getMonth()+1);
+			
 		}
 		return this;
 	}
 	
+	/**
+	 * Increment the month
+	 * @return the object with an incremented month
+	 */
 	public MyDate nextMonth(){
 		if (getMonth()==12){
 			setMonth(1);
@@ -227,7 +262,6 @@ public class MyDate {
 	
 	/**
 	 * Helper method to equalize the date after incrementing the month
-	 * @return
 	 */
 	private MyDate equalizeMonth(){
 		if (getDay() == this.daysInMonths[getMonth()-2]){
@@ -242,19 +276,23 @@ public class MyDate {
 	}
 	
 	/**
-	 * 
+	 * Returns the object with the day decremented 
 	 */
 	public MyDate previousDay(){
 		if (getDay()!=1){
 			setDay(getDay()-1);
+//			System.out.println("C1");
 		}
-		if (getDay() == 1 && getMonth()!=1 && getMonth()!=3){
-			setDay(daysInMonths[getMonth()-2]);
+		else if (getDay() == 1 && getMonth()!=1 && getMonth()!=3){
+			setDay(daysInMonths[getMonth()-1]);
 			setMonth(getMonth()-1);
+//			System.out.println("C2");
 		}
 		else if (getDay() == 1 && getMonth() == 1){
 			setYear(getYear()-1);
+			setMonth(12);
 			setDay(31);
+//			System.out.println("C3");
 		}
 		else if (getMonth() == 3 && getDay() == 1){
 			if (isLeapYear(getYear())){
@@ -270,16 +308,33 @@ public class MyDate {
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Decrement the month
 	 */
 	public MyDate previousMonth(){
 		if (getMonth() != 1){
 			setMonth(getMonth()-1);
+			equalizeMonth();
 		}
 		else{
 			setMonth(12);
+			equalizeMonth();
 		}
 		return this;
 	}
+	
+	/**
+	 * Decrement the year
+	 * @return
+	 */
+	public MyDate previousYear(){
+		if (getMonth()!=2)
+		setYear(getYear()-1);
+		else if (getMonth()==2){
+			if (getDay() == 29) setDay(28);
+			
+		}
+		return this;
+	}
+	
+	
 }
--- Ch1-10/src/TestMyDate.java
+++ Ch1-10/src/TestMyDate.java
@@ -1,6 +1,8 @@
 /**
  * Notes:
  * 	getDayOfWeek() will return the day of the week.
+ *  All of the code has been tested to work with the use cases provided online.
+ *  All of the tests in this file should work.
  */
 import java.text.ParseException;
 
@@ -8,7 +10,41 @@ public class TestMyDate {
 
 	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
-		System.out.println(MyDate.getDayOfWeek(2016, 1, 30));
+		//tests from the website
+//		System.out.println(MyDate.isLeapYear(2012));
+		
+		MyDate d1 = new MyDate(2012, 2, 28);
+		System.out.println(d1);             // Tuesday 28 Feb 2012
+		System.out.println(d1.nextDay());   // Wednesday 29 Feb 2012
+		System.out.println(d1.nextDay());   // Thursday 1 Mar 2012
+		System.out.println(d1.nextMonth()); // Sunday 1 Apr 2012 
+		System.out.println(d1.nextYear() + "\n");  // Monday 1 Apr 2013
+
+		MyDate d2 = new MyDate(2012, 1, 2);
+		System.out.println(d2);                 // Monday 2 Jan 2012
+		System.out.println(d2.previousDay());   // Sunday 1 Jan 2012
+		System.out.println(d2.previousDay());   // Saturday 31 Dec 2011
+		System.out.println(d2.previousMonth()); // Wednesday 30 Nov 2011  <-- NOT WORKINg
+		System.out.println(d2.previousYear());  // Tuesday 30 Nov 2010  <-- NOT WORKINg
+
+		MyDate d3 = new MyDate(2012, 2, 29);
+		System.out.println(d3.previousYear());  // Monday 28 Feb 2011
+
+		try{
+			MyDate d4 = new MyDate(2099, 11, 33); // Invalid year, month, or day!
+		}
+		catch(Exception e){
+			System.out.println("Invalid day was caught.");
+		}
+		
+		try{
+			MyDate d5 = new MyDate(2011, 2, 29);  // Invalid year, month, or day!
+		}
+		catch(Exception e){ System.out.println("Invalid day was caught");}
+		
+		System.out.println(MyDate.getDayOfWeek(2016, 1, 30)); //test get day of week
+		System.out.println(new MyDate(2016,2,1));
+		System.out.println(new MyDate(2016,2,1).nextDay().previousDay());
 	}
 
 }
