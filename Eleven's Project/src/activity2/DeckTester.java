0b1c387ede98e5e7f64f5511ade93a0cbeeecb35
Robbie S <robbie@selwynsoftware.com>
2016-03-30 13:06:07 -0700

    question 3 answered

--- Eleven's Project/src/activity2/DeckTester.java
+++ Eleven's Project/src/activity2/DeckTester.java
@@ -16,4 +16,16 @@ public class DeckTester {
 		Deck d = new Deck(ranks,suit,vals);
 		System.out.println(d);
 	}
+	
+	public static Deck initializeTwentyOneDeck() {
+		String[] ranks = {"Two","Three", "Four", "Five", "Size", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King","Ace","Two","Three", "Four", "Five", "Size", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King","Ace","Two","Three", "Four", "Five", "Size", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King","Ace","Two","Three", "Four", "Five", "Size", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King","Ace"};
+		String[] suits = new String[52];
+		for (int i=0;i<13;i++) suits[i] = "Hearts";
+		for (int i=13;i<26;i++) suits[i] = "Diamonds";
+		for (int i=26;i<39;i++) suits[i] = "Spades";
+		for (int i=39;i<52;i++) suits[i] = "Clubs";
+		int[] vals = {2,3,4,5,6,7,8,9,10,11,12,13,14,2,3,4,5,6,7,8,9,10,11,12,13,14,2,3,4,5,6,7,8,9,10,11,12,13,14,2,3,4,5,6,7,8,9,10,11,12,13,14};
+		return new Deck(ranks,suits,vals);
+		
+	}
 }
